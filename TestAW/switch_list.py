23fe4e6cbb3aa9bf2fb38a8664735827902cb7c2
duwei <nkdruw@163.com>
2018-12-24 20:35:41 +0800

    add file

--- TestAW/switch_list.py
+++ TestAW/switch_list.py
@@ -12,7 +12,7 @@ def switch_list(list1,list2):
 
     c_list = list1 + list2
     all_combine = list(itertools.combinations(c_list,len(c_list)//2))
-    list1 = min(all_combine,key=lambda x:abs(sum(x)-sum(c_list)/2))
+    list1 = list(min(all_combine,key=lambda x:abs(sum(x)-sum(c_list)/2)))
     list2 = c_list
     for ele in list1:
         list2.remove(ele)
--- test_case/test_case2.py
+++ test_case/test_case2.py
@@ -9,12 +9,13 @@ class switchlist(unittest.TestCase):
         self.b = []
 
     def tearDown(self):
-        pass
+        del self.a[:]
+        del self.b[:]
 
     def test1(self):
         list1 = [2,3,4,5]
         list2 = [1,3,2,1]
-        self.a,self.b =switch_list(list1,list2)
+        self.a,self.b = switch_list(list1,list2)
         self.assertEqual(abs(sum(self.a)-sum(self.b)),1)
 
     def test2(self):
