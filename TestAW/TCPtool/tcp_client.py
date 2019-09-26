74e83b8c06936c2efa2058cfc8f0478f91521f51
nkdruw@163.com <Qwe*90op19870707adu>
2019-09-26 18:00:56 +0800

    change format

--- TestAW/TCPtool/tcp_client.py
+++ TestAW/TCPtool/tcp_client.py
@@ -1,44 +1,45 @@
-#coding=utf-8
+# coding=utf-8
 
 import socket
+
 
 class tcpClient():
 
     def __init__(self):
 
-        self.s =None
+        self.s = None
 
-    def connect(self,*args):
-        #建立socket
-        self.s =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        #连接
+    def connect(self, *args):
+        # 建立socket
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # 连接
         self.s.connect(args)
 
     def disconect(self):
         self.s.close()
 
-    def sendMsg(self,msg):
+    def sendMsg(self, msg):
         self.s.send(msg)
 
     def recvMsg(self):
 
-        Buffer =[]
+        Buffer = []
         while True:
-            data =self.s.recv(1024)
+            data = self.s.recv(1024)
             if data:
                 Buffer.append(data)
             else:
                 break
-        data =b''.join(Buffer)
+        data = b''.join(Buffer)
         return data
 
-if __name__ == '__main__':
 
-    s=tcpClient()
-    s.connect('127.0.0.1',8080)
+if __name__ == '__main__':
+    s = tcpClient()
+    s.connect('127.0.0.1', 8080)
     print s.recvMsg()
 
     s.sendMsg(b'Hanmeimei')
-    recData =s.recvMsg()
+    recData = s.recvMsg()
     print recData
     s.disconect()
--- TestAW/TCPtool/tcp_server.py
+++ TestAW/TCPtool/tcp_server.py
@@ -1,39 +1,41 @@
-#coding=utf-8
+# coding=utf-8
 import socket
 import threading
 import time
 
+
 class tcpServer():
 
     def __init__(self):
-        self.s =None
+        self.s = None
 
     def startServer(self):
-        self.s =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.s.bind(('127.0.0.1',8080))
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind(('127.0.0.1', 8080))
         self.s.listen(5)
         print "TCP Server '127.0.0.1' started..."
 
     def recvClient(self):
-        sock,addr =self.s.accept()
-        t =threading.Thread(target=self.msgInteractive,args=(sock,addr))
+        sock, addr = self.s.accept()
+        t = threading.Thread(target=self.msgInteractive, args=(sock, addr))
         t.start()
 
-    def msgInteractive(self,sock,addr):
-        print 'Accept new connection from %s' %addr
+    def msgInteractive(self, sock, addr):
+        print 'Accept new connection from %s' % addr
         sock.send('Welcome to MyServer!')
         while True:
-            data =sock.recv(1024)
+            data = sock.recv(1024)
             time.sleep(1)
-            if not data or data.decode('utf-8') =='exit':
+            if not data or data.decode('utf-8') == 'exit':
                 break
             else:
                 sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
         sock.close()
-        print 'Connection from %s closed!' %addr
+        print 'Connection from %s closed!' % addr
 
-if __name__ =='__main__':
-    s =tcpServer()
+
+if __name__ == '__main__':
+    s = tcpServer()
     s.startServer()
     while True:
         s.recvClient()
